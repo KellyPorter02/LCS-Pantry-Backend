7074c4eed14841734fc1d4966d458d4caeaa0112
Kelly A Porter <78833247+KellyPorter02@users.noreply.github.com>
2021-05-03 21:02:05 -0400

    initial commit

---
+++ src/main/java/com/inventorybackend/LCS_inventory/LcsInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.inventorybackend.LCS_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcsInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcsInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventorybackend/LCS_inventory/LcsInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventorybackend.LCS_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcsInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
